273ea4ae72736f0a9a678f79ffc83ea17a30a53d
Guillermo <Guillermo@example.com>
2017-05-11 19:41:02 +0000

    Info from vars in the motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,10 +5,10 @@ end
 file '/etc/motd' do
    content "Property of Guillermo
 
-   IPAddress:	75.101.250.119
-   HOSTNAME:	ip-10-86-163-156
-   MEMORY:	604192 kB
-   CPU:		2266.746 MHz
+   IPAddress:	#{node['ipaddress']}
+   HOSTNAME:	#{node['hostname']}
+   MEMORY:	#{node['memory']['total']}
+   CPU:		#{node['cpu']['0']['mhz']}
 
 "
 
